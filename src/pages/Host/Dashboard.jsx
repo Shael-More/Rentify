55815f605b766929a941f6becc489fd94e99fcde
Shael more <89303457+Shael-More@users.noreply.github.com>
2026-06-10 21:29:00 +0530

    updated component

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -68,34 +68,3 @@ export default function Dashboard() {
     </>
   );
 }
-
-// import React from 'react'
-// import { Link } from 'react-router-dom';
-
-// const Dashboard = () => {
-//   return (
-//     <div>
-//       <section className='host-dashboard-earnings'>
-//         <div className='info'>
-//           <h1>Welcome!</h1>
-
-//           <p>
-//             Income last <span>30 days</span>
-//           </p>
-//           <h2>$2,260</h2>
-//         </div>
-//         <Link to='income'>Details</Link>
-//       </section>
-//       <section className='host-dashboard-reviews'>
-//         <h2>Review score</h2>
-//         {/* <BsStarFill className='star' /> */}
-//         <p>
-//           <span>5.0</span>/5 //{' '}
-//         </p>
-//         <Link to='reviews'>Details</Link>
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default Dashboard;
